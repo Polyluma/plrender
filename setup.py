7ebfe1502737cf4ec04bc2afe6e96089de633518
disphenoid <j@mono.io>
2019-10-05 21:20:38 +0200

    changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='plrender',
-    version='0.0.1',
+    version='0.0.2',
     description='Polyluma blender render',
     url='git@github.com:Polyluma/plrender.git',
     author='Johannes Schacht',
